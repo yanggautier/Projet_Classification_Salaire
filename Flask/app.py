66d3995762e6b53a1469e657d502db6b89d29800
Salim <salim.okar@live.fr>
2020-05-29 02:16:56 +0200

    fourth commit

--- Flask/app.py
+++ Flask/app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+import oyaml as yaml
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
-# app.config['JSON_SORT_KEYS'] = False
+app.config['JSON_SORT_KEYS'] = False
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    websitedata = yaml.load(open('Flask/_config.yaml', encoding='utf-8'))
+
+    return render_template('index.html', data=websitedata)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
